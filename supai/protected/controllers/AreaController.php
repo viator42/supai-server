9b852769f7381881350b6e3337bee2bae20decdf
Viator42 <via313@gmail.com>
2015-02-24 15:56:28 +0800

    地区area 控制器完成

--- supai/protected/controllers/AreaController.php
+++ supai/protected/controllers/AreaController.php
@@ -1,10 +1,66 @@
 <?php
-
+/*
+地区控制器
+*/
 class AreaController extends Controller
 {
+	
+	public function accessRules()
+    {
+        return array(
+            array('allow',  // allow all users to perform 'index' and 'view' actions
+                'users'=>array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+//                'actions'=>array('*'),
+                'users'=>array('@'),
+            ),
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
+    
+
 	public function actionIndex()
 	{
 		$this->render('index');
+	}
+
+	//省列表
+	public function actionProvinces()
+	{
+		$result = array();
+
+		$areas = Area::model()->findAll('level = 0');
+
+		foreach ($areas as $area) 
+		{
+			$result[] = $area;
+
+		}
+
+		$json = CJSON::encode($result);
+        echo $json;
+	}
+
+	//根据code查找下一级
+	public function actionChildren()
+	{
+		$result = array();
+
+		$code = $_GET['code'];
+
+		$cities = Area::model()->findAll('p_code=:p_code', array(':p_code' => $code));
+
+		foreach ($cities as $city) 
+		{
+			$result[] = $city;
+
+		}
+
+		$json = CJSON::encode($result);
+        echo $json;
 	}
 
 	// Uncomment the following methods and override them if needed
