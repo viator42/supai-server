d30920d9d1b63e8dae90b71f9a98ca8fc5b0794b
Viator42 <via313@gmail.com>
2015-03-06 18:04:37 +0800

    添加ignore文件

--- supai/protected/controllers/OrderController.php
+++ supai/protected/controllers/OrderController.php
@@ -1,10 +1,30 @@
 <?php
 
+//订单
 class OrderController extends Controller
 {
 	public function actionIndex()
 	{
 		$this->render('index');
+	}
+
+
+	//商户查看已提交的订单
+	public function actionActiveOrdersForMerchant()
+	{
+		$result = array();
+
+		$merchantId = $_POST['id'];
+		// 1 已提交 		2 已发货
+		$orders = Order::model()->find('merchant_id = :merchantId and (status = 1 or status = 2)', array(':merchantId'=>$merchantId));
+		foreach ($orders as $order) 
+		{
+			$result[] = $order;
+
+		}
+
+		$json = CJSON::encode($result);
+        echo $json;
 	}
 
 	// Uncomment the following methods and override them if needed
