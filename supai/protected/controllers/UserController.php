6b6d27f150d3fee9525190e5322f0f2e10c90a7e
Viator42 <via313@gmail.com>
2015-03-09 13:29:02 +0800

    登录/注册

--- supai/protected/controllers/UserController.php
+++ supai/protected/controllers/UserController.php
@@ -2,46 +2,106 @@
 
 class UserController extends Controller
 {
+	public function accessRules()
+    {
+        return array(
+            array('allow',  // allow all users to perform 'index' and 'view' actions
+                'users'=>array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+//                'actions'=>array('*'),
+                'users'=>array('@'),
+            ),
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
+
 	public function actionIndex()
 	{
 		$this->render('index');
 	}
 
+
 	//登录
 	public function actionLogin()
 	{
-		$result = array('success'=>false,
-                         'err_msg'=>'',
-                         'data'=>null);
+		$result = array('success'=>false);
 
-		if(isset($_POST['username']) && isset($_POST['password']))
+		if(isset($_POST['tel']) && isset($_POST['password']))
 		{
-			$username = $_POST['username'];
+			$tel = $_POST['tel'];
 			$password = $_POST['password'];
 
-			
+			$user = User::model()->find('tel = :tel & password = :password', array(':tel'=>$tel, $password=>$password));
 
-			result['success'] = true;
-		}
-		else
-		{
-			result['err_msg'] = "参数错误";
-			result['success'] = false;
+			if($user != null)
+			{
+				result['data'] = $user;				
+				result['success'] = true;
+			}
 
 		}
 
 		$json = CJSON::encode($result);
         echo $json;
 
-		
-
-
 	}
 
 	//注册
 	public function actionRegister()
 	{
+		$result = array('success'=>false);
 
+		if(isset($_POST['tel']) && isset($_POST['password']))
+		{
+			$tel = $_POST['tel'];
+			$password = $_POST['password'];
+
+			&model = new User();
+
+			$model->username = $tel;
+			$model->tel = $tel;
+			$model->password = $password;
+			$model->register_time = time();
+
+			&model->save();
+
+			result['data'] = $model;	
+			result['success'] = true;
+		}
+
+		$json = CJSON::encode($result);
+        echo $json;
+
+	}
+
+	//完善用户信息
+	public function actionPerfection()
+	{
+		$result = array('success'=>false);
+
+		if (isset($_POST['id'])) 
+		{
+			$id = $_POST['id'];
+
+			$user = User::model->findByPk($id);
+			if($user != null)
+			{
+				$user->name = $_POST['name'];
+				$user->area_id = $_POST['area'];
+				$user->address = $_POST['address'];
+
+				$user->save();
+				result['success'] = true;
+
+			}
+
+		}
+
+		$json = CJSON::encode($result);
+        echo $json;
 	}
 
 	// Uncomment the following methods and override them if needed
