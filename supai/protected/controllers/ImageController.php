95e7021d3e6d394794af7b1b54d430ead8c58cc1
Viator42 <via313@gmail.com>
2015-04-02 09:42:50 +0800

    图片上传修改

--- supai/protected/controllers/ImageController.php
+++ supai/protected/controllers/ImageController.php
@@ -7,6 +7,22 @@ class ImageController extends Controller
 		$this->render('index');
 	}
 
+	public function accessRules()
+    {
+        return array(
+            array('allow',  // allow all users to perform 'index' and 'view' actions
+                'users'=>array('*'),
+            ),
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+//                'actions'=>array('*'),
+                'users'=>array('@'),
+            ),
+            array('deny',  // deny all users
+                'users'=>array('*'),
+            ),
+        );
+    }
+
 	public function actionUpload()
 	{
 		$result = array('success'=>false);
