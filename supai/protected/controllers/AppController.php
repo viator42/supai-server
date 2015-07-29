16faf9d9a522becbba4f3480c19800e405fab6e0
Viator42 <via313@gmail.com>
2015-07-29 12:36:34 +0800

    bug fix

---
+++ supai/protected/controllers/AppController.php
@@ -0,0 +1,48 @@
+<?php
+
+class AppController extends Controller
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+
+    // 获取应用信息
+    public function actionInfo()
+    {
+        $result = array();
+
+        $result['versionName'] = "1.1.2";
+        $result['versionCode'] = 5;
+
+        $json = CJSON::encode($result);
+        echo $json;
+    }
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
